6ee81df73ef02bab432da4e13556233a961e155d
Edwin Cardenas <eacardenase@unal.edu.co>
2023-04-14 11:55:20 -0500

    feat: variables

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    echo '<a href="00_syntax.php">00_syntax.php</a>';
+    echo '<br/>';
+    echo '<a href="01_comments.php">01_comments.php<a/>';
+    echo '<br/>';
+    echo '<a href="02_variables.php">02_variables.php<a/>';
+    echo '<br/>';
+    echo '<a href="03_numbers.php">03_numbers.php<a/>';
+    echo '<br/>';
+    echo '<a href="04_strings.php">04_strings.php<a/>';
+    echo '<br/>';
+    echo '<a href="05_arrays.php">05_arrays.php<a/>';
+    echo '<br/>';
+    echo '<a href="06_conditionals.php">06_conditionals.php<a/>';
+    echo '<br/>';
+    echo '<a href="07_loops.php">07_loops.php<a/>';
+    echo '<br/>';
+    echo '<a href="08_functions.php">08_functions.php<a/>';
+    echo '<br/>';
+    echo '<a href="09_dates.php">09_dates.php<a/>';
+?>
