7e33901a820014b858687809fcf1658a32a27f80
Edwin Cardenas <eacardenase@unal.edu.co>
2023-04-17 11:42:55 -0500

    feat: basic oop

--- 12_oop/index.php
+++ 12_oop/index.php
@@ -2,8 +2,61 @@
 
 // What is class and instance
 
+class Person {
+    public $name;
+    public $surname;
+    private $age;
+    public static $counter = 0;
+
+    // constructor function
+    public function __construct(string $name, string $surname, int $age)
+    {
+        // this is for the instance
+        $this->name = $name;
+        $this->surname = $surname;
+        $this->age = $age;
+
+        // self is for the class itself
+        self::$counter++;
+    }
+
+    // Using setter and getter
+    public function setAge(int $age): void
+    {
+        $this->age = $age;
+    }
+
+    public function getAge(): int
+    {
+        return $this->age;
+    }
+
+    public static function getCounter(): int
+    {
+        return self::$counter;
+    }
+}
+
 // Create Person class in Person.php
+
+$person = New Person('Edwin', 'Cardenas', 27);
+
+$person -> setAge(30);
+
+echo '<pre>';
+var_dump($person);
+echo '</pre>';
+
+echo $person->getAge();
+
+echo '<br/>';
 
 // Create instance of Person
 
-// Using setter and getter
+$person2 = New Person('Ana', 'Torres', 25);
+
+echo Person::$counter;
+
+echo '<br/>';
+
+echo Person::getCounter();
