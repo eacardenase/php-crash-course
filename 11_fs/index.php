374905a80b1a3f94b953012c63ecdae91b3d70aa
Edwin Cardenas <eacardenase@unal.edu.co>
2023-04-17 10:15:37 -0500

    feat: working with files

--- 11_fs/index.php
+++ 11_fs/index.php
@@ -1,21 +1,58 @@
 <?php
 // Magic constants
 
+echo __DIR__.'<br/>';
+echo __FILE__.'<br/>';
+echo __LINE__.'<br/>';
+
 // Create directory
+
+//mkdir('test');
 
 // Rename directory
 
+//rename('test', 'test2');
+
 // Delete directory
+
+//rmdir('test2');
 
 // Read files and folders inside directory
 
+//$files = scandir('./');
+//
+//echo '<pre>';
+//var_dump($files);
+//echo '</pre>';
+
 // file_get_contents, file_put_contents
+
+//echo file_get_contents('lorem.txt');
+//file_put_contents('sample.txt', 'test');
 
 // file_get_contents from URL
 
+//$usersJson = file_get_contents('https://jsonplaceholder.typicode.com/users');
+
+//echo '<pre>';
+//var_dump($usersJson);
+//echo '</pre>';
+
+//$users = json_decode($usersJson, true);
+//
+//echo '<pre>';
+//var_dump($users);
+//echo '</pre>';
+
 // https://www.php.net/manual/en/book.filesystem.php
 // file_exists
+
+echo file_exists('sample.txt'); // true converted to 1
+
 // is_dir
+
+echo is_dir('test');
+
 // filemtime
 // filesize
 // disk_free_space
