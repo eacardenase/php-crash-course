954652465dc617ba8a18f659bbfffa495deb2fa4
Edwin Cardenas <eacardenase@unal.edu.co>
2023-04-17 09:59:51 -0500

    feat: working with dates

--- 09_dates.php
+++ 09_dates.php
@@ -2,12 +2,38 @@
 
 // Print current date
 
+echo date('Y-m-d H:i:s');
+echo '<br/>';
+
 // Print yesterday
+
+echo date('Y-m-d H:i:s', time() - 60 * 60 * 24);
+echo '<br/>';
 
 // Different format: https://www.php.net/manual/en/function.date.php
 
+echo date('F j Y, H:i:s');
+echo '<br/>';
+
 // Print current timestamp
+
+echo time();
+echo '<br/>';
 
 // Parse date: https://www.php.net/manual/en/function.date-parse.php
 
+//$parsedDate = date_parse('2020-10-12 09:00:00');
+//
+//echo '<pre>';
+//var_dump($parsedDate);
+//echo '</pre>';
+
 // Parse date from format: https://www.php.net/manual/en/function.date-parse-from-format.php
+
+$dateString = 'April 17 2023 12:45:35';
+
+$parsedDate = date_parse_from_format('F j Y H:i:s', $dateString);
+
+echo '<pre>';
+var_dump($parsedDate);
+echo '</pre>';
